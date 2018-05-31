dd4ed6b97e343384ee9247fd1e5153a91f8e4406
Zain Syed <zsyed@registers.maryland.gov>
2018-05-31 11:00:09 -0400

    Recipe with client

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import 'rxjs/Rx';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private httpClient: HttpClient,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
+
+  storeRecipes() {
+    const token = this.authService.getToken();
+
+    return this.httpClient.put('https://ng-recipe-book-a1202.firebaseio.com/recipes.json?auth=' + 
+    token, this.recipeService.getRecipes());
+    
+  }
+
+  getRecipes() {
+    const token = this.authService.getToken();
+
+    //this.httpClient.get<Recipe[]>('https://ng-recipe-book-a1202.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get('https://ng-recipe-book-a1202.firebaseio.com/recipes.json?auth=' + token, 
+    {
+      observe: 'response',
+      responseType: 'blob'
+    })
+      .map(
+        ( recipes) => {
+          console.log(recipes);
+          /* for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          } */
+          //return recipes;
+          return [];
+          
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
+  }
+}
